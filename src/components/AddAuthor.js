62351e1dd13e87b353203bb5423bb5fd7db546b4
Yosra <yos.skhiri@gmail.com>
2020-11-19 13:05:16 +0100

    added a msg to the author form to indicate if the form was successfully submitted or not

--- src/components/AddAuthor.js
+++ src/components/AddAuthor.js
@@ -11,10 +11,12 @@ const AddAuthor = () => {
         }
     );
 
-    const [{src, alt}, setPreview] = useState({
+    const [{ src, alt }, setPreview] = useState({
         src: placeholder,
         alt: 'Default'
     });
+
+    const [msg, setMsg] = useState();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -22,23 +24,23 @@ const AddAuthor = () => {
         formData.append('photo', newAuthor.photo);
         formData.append('birthdate', newAuthor.birthdate);
         formData.append('name', newAuthor.name);
-        
+
         axios.post('http://localhost:5000/authors/add/', formData)
-             .then(res => {
-                console.log(res);
-             })
-             .catch(err => {
-                console.log(err);
-             });
+            .then(res => {
+                setMsg('The Author has been added');
+            })
+            .catch(err => {
+                setMsg('Something went wrong, Please try again!');
+            });
     }
 
     const handleChange = (e) => {
-        setNewAuthor({...newAuthor, [e.target.name]: e.target.value});
+        setNewAuthor({ ...newAuthor, [e.target.name]: e.target.value });
     }
 
     const handlePhoto = (e) => {
-        setNewAuthor({...newAuthor, photo: e.target.files[0]});    
-        if(e.target.files[0]) {
+        setNewAuthor({ ...newAuthor, photo: e.target.files[0] });
+        if (e.target.files[0]) {
             setPreview({
                 src: URL.createObjectURL(e.target.files[0]),
                 alt: e.target.files[0].name
@@ -47,52 +49,57 @@ const AddAuthor = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit} encType='multipart/form-data' className="author__form">
-            <div className="flex">
-                <div className="flex__1">
-                    <input 
-                        type="file" 
-                        accept=".png, .jpg, .jpeg"
-                        name="photo"
-                        id="photo"
-                        className="visually-hidden"
-                        onChange={handlePhoto}
-                    />
-                    <label htmlFor="photo" className="add-pic">
-                        <svg width="150" height="150" viewBox="0 0 24 24" fill="none" stroke="#459db7" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3"/>
-                            <circle cx="12" cy="10" r="3"/>
-                            <circle cx="12" cy="12" r="10"/>
-                        </svg>
-                    </label>
-                    <img className="preview-pic" src={src} alt={alt}/>
-                </div>
-                <div className="inputs">
-                    <input 
-                        type="text"
-                        placeholder="name"
-                        name="name"
-                        className="input-txt input-txt--title"
-                        value={newAuthor.name}
-                        onChange={handleChange}
-                    />
+        <div>
+            <form onSubmit={handleSubmit} encType='multipart/form-data' className="author__form">
+                <div className="flex">
+                    <div className="flex__1">
+                        <input
+                            type="file"
+                            accept=".png, .jpg, .jpeg"
+                            name="photo"
+                            id="photo"
+                            className="visually-hidden"
+                            onChange={handlePhoto}
+                        />
+                        <label htmlFor="photo" className="add-pic">
+                            <svg width="150" height="150" viewBox="0 0 24 24" fill="none" stroke="#459db7" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                                <path d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
+                                <circle cx="12" cy="10" r="3" />
+                                <circle cx="12" cy="12" r="10" />
+                            </svg>
+                        </label>
+                        <img className="preview-pic" src={src} alt={alt} />
+                    </div>
+                    <div className="inputs">
+                        <input
+                            type="text"
+                            placeholder="name"
+                            name="name"
+                            className="input-txt input-txt--title"
+                            value={newAuthor.name}
+                            onChange={handleChange}
+                        />
 
-                    <input 
-                        type="date"
-                        name="birthdate"
-                        className="input-txt"
-                        value={newAuthor.date}
-                        onChange={handleChange}
-                    />
-                    <input 
-                        type="submit"
-                        value="Add Author"
-                        className="btn btn-primary submit"
-                    />
+                        <input
+                            type="date"
+                            name="birthdate"
+                            className="input-txt"
+                            value={newAuthor.date}
+                            onChange={handleChange}
+                        />
+                        <input
+                            type="submit"
+                            value="Add Author"
+                            className="btn btn-primary submit"
+                        />
+                    </div>
                 </div>
-            </div>
+            </form>
+            {
+                msg ? <div className="msg">{msg}</div> : null
+            }
             
-        </form>
+        </div>
     );
 }
 
